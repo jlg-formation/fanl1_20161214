3087bde04340e7f6fdc3275879aa40d86fb222fa
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-12-13 11:48:00 +0100

    ors star step 4

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -9,20 +9,24 @@
 			link: function(scope, element, attrs) {
 				console.log('orsStar link', arguments);
 				
-				var html = '';
-				var note = attrs.note;
-				console.log('note', note);
-				note = (note === undefined || note === '') ? 2 : note;
-				note = (scope[note] === undefined) ? note : scope[note];
-				note = Number(note);
-				note = (isNaN(note)) ? 2 : note;
-				for (var i = 0; i < note; i++) {
-					html += '<img src="ors-star/img/yellow_star.png" />';
-				}
-				for (var i = note; i < 5; i++) {
-					html += '<img src="ors-star/img/white_star.png" />';
-				}
-				element.html(html);
+				scope.$watch('myNote', function() {
+					var html = '';
+					var note = attrs.note;
+					console.log('note', note);
+					note = (note === undefined || note === '') ? 2 : note;
+					note = (scope[note] === undefined) ? note : scope[note];
+					note = Number(note);
+					note = (isNaN(note)) ? 2 : note;
+					for (var i = 0; i < note; i++) {
+						html += '<img src="ors-star/img/yellow_star.png" />';
+					}
+					for (var i = note; i < 5; i++) {
+						html += '<img src="ors-star/img/white_star.png" />';
+					}
+					element.html(html);
+				});
+				
+				
 			}
 		};
 	});
