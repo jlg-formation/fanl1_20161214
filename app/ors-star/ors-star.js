b5436b7a458efd4a7997500e72797f21aa79a3b1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-12-13 12:07:41 +0100

    ors star step 5

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -6,17 +6,24 @@
 	app.directive('orsStar', function() {
 		return {
 			restrict: 'E',
+			scope: {
+				n: '=note'
+			},
 			link: function(scope, element, attrs) {
 				console.log('orsStar link', arguments);
+				scope['3'] = 1;
 				
-				scope.$watch('myNote', function() {
+				scope.$watch('n', function() {
 					var html = '';
-					var note = attrs.note;
+					var note = scope.n;
+					
 					console.log('note', note);
+					
 					note = (note === undefined || note === '') ? 2 : note;
-					note = (scope[note] === undefined) ? note : scope[note];
+					
 					note = Number(note);
 					note = (isNaN(note)) ? 2 : note;
+					
 					for (var i = 0; i < note; i++) {
 						html += '<img src="ors-star/img/yellow_star.png" />';
 					}
